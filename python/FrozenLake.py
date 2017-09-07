834c2eb1d4a8a16b03ec0d76a3f56382d469118b
eddiegame <31731158+eddiegame@users.noreply.github.com>
2017-09-07 13:39:59 +0200

    Add files via upload

---
+++ python/FrozenLake.py
@@ -0,0 +1,87 @@
+#-*- coding: utf-8 -*-
+'''
+Created on 01.09.2017
+@author: steffl&flemmig
+'''
+
+import gym
+import numpy as np
+''' Fuer die Matritzen '''
+import random 
+''' Fuer die Zufallszahlen'''
+import NeuralNetwork as nn
+
+if __name__ == '__main__':
+    pass
+
+
+print("loading Environment..")
+env = gym.make('FrozenLake-v0')
+
+print("loading Spaces..")
+InputSum = env.observation_space
+Inputlenght = 16
+''' Discrete(4)'''
+''' Das ist die Anzahl der Moeglichen Aktionen'''
+''' Discrete = {0,1}'''
+OutputSum = env.action_space
+Outputlength = 4
+''' Discrete(16)'''
+''' das ist die Map also der State/Observation'''
+
+print("initializing weights and nodes matrix..")
+amountNodesHL = Inputlenght + 1
+''' Anzahl der Nodes je Hidden Layer'''
+#weights = [np.zeros((amountNodesHL,Inputlenght)),np.zeros((amountNodesHL,amountNodesHL)),np.zeros((Outputlength,amountNodesHL ))]
+weights = [np.zeros((amountNodesHL,Inputlenght)),np.zeros((amountNodesHL,amountNodesHL)),np.zeros((Outputlength,amountNodesHL ))]
+# weights ist die Matrix mit den Weights[Layer][Ziel][Quelle]
+# Sie wird mit den Anzahlen der Nodes initialisiert (InputAmount, InputAmount+1,InputAmount+1,OutputAmount)
+nodes = [np.zeros(Inputlenght),np.zeros(amountNodesHL),np.zeros(amountNodesHL),np.zeros(Outputlength)]
+absnodes = [np.zeros(Inputlenght),np.zeros(amountNodesHL),np.zeros(amountNodesHL),np.zeros(Outputlength)]
+# Nodes ist die Matrix mit den Nodewerten[Layer][Node] 
+# Sie wird mit der Anzahl der Nodes initialisiert
+
+print("filling weights with random..")
+#init weights
+for i in range(3):
+    for x in range(len(weights[i])):
+        for y in range(len(weights[i][x])):
+            weights[i][x][y] = random.uniform(0, 1)
+            '''print(weights[i][x][y])'''
+
+    
+    
+print("weights randomly initliaized")
+#print (weights)
+
+# Looooooooooop
+episodes = 500
+
+# Variables
+lear_rate   = 0.1
+muta_rate   = 0.3
+gamma       = 0.9
+observation = env.reset()
+
+if(nn.debug):
+    print("Weights")
+    print(weights[0])
+    print ()
+    print(weights[1])
+    print ()
+    print(weights[2])
+    print ()
+
+nn.Qvalue(weights,nodes,absnodes,observation)
+'''
+for e in range(episodes):
+    observation = env.reset()
+    done = False
+    moves = 0
+    while(done == False):
+        moves+=1
+        if(random.uniform() <= muta_rate):
+            action = env.action_space.sample()
+        else:
+            action = nn.Qvalue(weights,observation)
+        env.step(action)'''
---
+++ python/NeuralNetwork.py
@@ -0,0 +1,59 @@
+'''
+Created on 02.09.2017
+@author: steffl&flemmig
+'''
+debug = True
+
+import numpy as np
+
+def sigmoid(x): return 1 / (1 + np.exp(-x))
+
+def Qvalue(weights, nodes, absnodes, observation):
+    # calculate Qvalue return the proper action
+    if(debug):
+        print("WeightMatrix")
+        print(weights)
+        print
+        print("NodeMatrix")
+        print(nodes)
+        print
+    
+    # 1. Einzelne Nodes berechnen Nodematrix erstellen
+    # Inputs aus Observation in den InputLayer übernehmen
+    nodes[0][observation]=1
+    if(debug):
+        print("Inputs in der NodeMatrix")
+        print(nodes)
+        print
+    # Berechne die Node Werte durch die Addition der Produkte aus weight und Node (beginne in Layer 1)
+    
+    # für jeden Layer (beginne mit 1)
+    layer = 1
+    while(layer < len(nodes)):
+        # für jeden Node im Layer layer
+        node = 0
+        while(node < len(nodes[layer])):
+            # erhöhe den wert in nodes[layer][node] um jedes Produkt der Weights und deren Nodes von denen sie kommen
+            weight = 0
+            while(weight < len(weights[layer-1][node])):
+                # aktueller node += weight des Pfades (layer-1) * node im vorherigen Layer von dem der Pfad kommt
+                absnodes[layer][node]+=(weights[layer-1][node][weight]*nodes[layer-1][weight])
+                nodes[layer][node] = sigmoid(absnodes[layer][node])
+                weight+=1
+            node+=1
+        layer+=1
+        
+    if(debug):
+        print("Node Werte berechnet:")
+        print(nodes)
+        print
+    # 2. Aktivierungsfunktion auf hidden und output Layer anwenden
+    
+    print(nodes)
+    
+    print()
+    # 3. Outputs berechnen anhand von state und state + 1
+    
+    # 4. state + action in matrix speichern
+    
+    
