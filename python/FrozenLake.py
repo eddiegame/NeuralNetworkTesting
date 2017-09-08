b116034d7f1dc6f81eff733023ff694bfae2e948
eddiegame <eddiegame1997@gmail.com>
2017-09-08 11:35:25 +0200

    kommentiert

--- python/FrozenLake.py
+++ python/FrozenLake.py
@@ -5,48 +5,50 @@ Created on 01.09.2017
 '''
 
 import gym
-import numpy as np
-''' Fuer die Matritzen '''
-import random 
-''' Fuer die Zufallszahlen'''
-import NeuralNetwork as nn
+import numpy as np 				#Notwendig für die Matritzenrechnung
+import random 					#Notwendig fpr die gernerierung von Zufallszahlen
+import NeuralNetwork as nn		#Beinhaltet das Neurale Netz
 
 if __name__ == '__main__':
     pass
 
-
+# Das Environment vom openai gym wird geladen (hier FrozenLake)
 print("loading Environment..")
 env = gym.make('FrozenLake-v0')
 
+# Die Spaces also die Anzahl verschiedener 
+# Observations und Actions wird aus dem Environment geladen
 print("loading Spaces..")
 InputSum = env.observation_space
-Inputlenght = 16
-''' Discrete(4)'''
-''' Das ist die Anzahl der Moeglichen Aktionen'''
-''' Discrete = {0,1}'''
+Inputlenght = 16				#Anzahl der Inputs (Observation)
 OutputSum = env.action_space
-Outputlength = 4
-''' Discrete(16)'''
-''' das ist die Map also der State/Observation'''
+Outputlength = 4				#Anzahl der Output (Actions)
+
 
 print("initializing weights and nodes matrix..")
 amountNodesHL = Inputlenght + 1
-''' Anzahl der Nodes je Hidden Layer'''
-#weights = [np.zeros((amountNodesHL,Inputlenght)),np.zeros((amountNodesHL,amountNodesHL)),np.zeros((Outputlength,amountNodesHL ))]
+# Die Weight Matix wird initialisiert
+# Weights[Layer][Ziel][Quelle]
+# weights = [np.zeros((amountNodesHL,Inputlenght)),np.zeros((amountNodesHL,amountNodesHL)),np.zeros((Outputlength,amountNodesHL ))]
 weights = [np.zeros((amountNodesHL,Inputlenght)),np.zeros((amountNodesHL,amountNodesHL)),np.zeros((Outputlength,amountNodesHL ))]
-# weights ist die Matrix mit den Weights[Layer][Ziel][Quelle]
-# Sie wird mit den Anzahlen der Nodes initialisiert (InputAmount, InputAmount+1,InputAmount+1,OutputAmount)
-nodes = [np.zeros(Inputlenght),np.zeros(amountNodesHL),np.zeros(amountNodesHL),np.zeros(Outputlength)]
-absnodes = [np.zeros(Inputlenght),np.zeros(amountNodesHL),np.zeros(amountNodesHL),np.zeros(Outputlength)]
-# Nodes ist die Matrix mit den Nodewerten[Layer][Node] 
-# Sie wird mit der Anzahl der Nodes initialisiert
 
-# Matrix für History
+# Die Nodematrix enthält die Nodewerte welche durch die Sigmoid Funktion normalisiert wurden
+# nodes[Layer][Node]
+nodes = [np.zeros(Inputlenght),np.zeros(amountNodesHL),np.zeros(amountNodesHL),np.zeros(Outputlength)]
+
+# Die AbsNodes Matrix enthält alle Nodewerte vor der Normalisierung mit der Sigmoid Funktion
+# absnodes[Layer][Node]
+absnodes = [np.zeros(Inputlenght),np.zeros(amountNodesHL),np.zeros(amountNodesHL),np.zeros(Outputlength)]
+
+# Die History Matrix enthält alle "gespielten" Actions, also Welche Action in welcher Observation wie oft vor kam
 history = []
 
 
+
+
+# Die Weight Matrix wird mit Zufallszahlen zwischen 0 und 1 gefüllt
+# falls diese Später abgespeichert werden sollte muss sie hier eingelesen werden
 print("filling weights with random..")
-#init weights
 for i in range(3):
     for x in range(len(weights[i])):
         for y in range(len(weights[i][x])):
@@ -54,19 +56,17 @@ for i in range(3):
             '''print(weights[i][x][y])'''
 
     
-    
 print("weights randomly initliaized")
-#print (weights)
 
-# Looooooooooop
+# Hier beginnt der main Loop
+# Variablen:
 episodes = 500
-
-# Variables
 lear_rate   = 0.1
 muta_rate   = 0.3
 gamma       = 0.9
 observation = env.reset()
 
+# Ein Debug print:
 if(nn.debug):
     print("Weights")
     print(weights[0])
@@ -75,26 +75,21 @@ if(nn.debug):
     print ()
     print(weights[2])
     print ()
+	
+# Training Loop
 for i in range(0 ,2):
+	# Das Environemnt wird dargestellt
     env.render()
+	# Die letzte observation wird gespeichert
     oldObs = observation
+	# Das Neurale Netz ermittelt die Action mit dem höchsten Q-Wert für den aktuellen Stand
     NextAction = nn.Qvalue(weights,nodes,absnodes,observation)
+	# Die beste Action wird ausgeführt
     observation, reward, done, info = env.step(NextAction)
+	# Die durchgeführte Action wird gespeichert
     newEntry = [oldObs,NextAction,observation,'1']
     history.append(newEntry)
     env.render()
     
+# Die history wird ausgegeben
 print(history)
-
-'''
-for e in range(episodes):
-    observation = env.reset()
-    done = False
-    moves = 0
-    while(done == False):
-        moves+=1
-        if(random.uniform() <= muta_rate):
-            action = env.action_space.sample()
-        else:
-            action = nn.Qvalue(weights,observation)
-        env.step(action)'''
